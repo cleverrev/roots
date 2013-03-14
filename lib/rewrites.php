072e48d1c454c62a29c11e11a7d20e6dfe298cea
Joel Kuczmarski <leoj3n@gmail.com>
2013-03-14 08:38:38 -0500

    child rewrites even in subdir

--- lib/rewrites.php
+++ lib/rewrites.php
@@ -18,26 +18,50 @@
 function roots_add_rewrites($content) {
   global $wp_rewrite;
   $roots_new_non_wp_rules = array(
-    'assets/css/(.*)'      => THEME_PATH . '/assets/css/$1',
-    'assets/js/(.*)'       => THEME_PATH . '/assets/js/$1',
-    'assets/img/(.*)'      => THEME_PATH . '/assets/img/$1',
+    // 'assets/css/(.*)'      => THEME_PATH . '/assets/css/$1',
+    // 'assets/js/(.*)'       => THEME_PATH . '/assets/js/$1',
+    // 'assets/img/(.*)'      => THEME_PATH . '/assets/img/$1',
     'plugins/(.*)'         => RELATIVE_PLUGIN_PATH . '/$1'
   );
   $wp_rewrite->non_wp_rules = array_merge($wp_rewrite->non_wp_rules, $roots_new_non_wp_rules);
   return $content;
 }
 
+function roots_add_conditions($content) {
+  $home_root = parse_url(home_url());
+  if ( isset( $home_root['path'] ) )
+    $home_root = trailingslashit($home_root['path']);
+  else
+    $home_root = '/';
+
+  $rules = array('RewriteRule ^index\.php$ - [L]');
+  
+  if (is_child_theme()) {
+    $rules[] = 'RewriteCond %{DOCUMENT_ROOT}/' . $home_root . CHILD_THEME_PATH . '/$1 -f';
+    $rules[] = 'RewriteRule ^(.*[^/])/?$ /' . $home_root . CHILD_THEME_PATH . '/$1 [QSA,L]';
+  }
+  
+  $rules[] = 'RewriteCond %{DOCUMENT_ROOT}/' . $home_root . THEME_PATH  . '/$1 -f';
+  $rules[] = 'RewriteRule ^(.*[^/])/?$ /' . $home_root . THEME_PATH . '/$1 [QSA,L]';
+    
+  return str_replace('RewriteRule ^index\.php$ - [L]', implode("\n", $rules), $content);
+}
+
 function roots_clean_urls($content) {
   if (strpos($content, FULL_RELATIVE_PLUGIN_PATH) === 0) {
     return str_replace(FULL_RELATIVE_PLUGIN_PATH, WP_BASE . '/plugins', $content);
   } else {
-    return str_replace('/' . THEME_PATH, '', $content);
+    if (is_child_theme())
+        $content = str_replace(unleadingslashit(CHILD_THEME_PATH), '', $content);
+
+    return untrailingslashit(str_replace(leadingslashit(THEME_PATH), '', $content));
   }
 }
 
-if (!is_multisite() && !is_child_theme() && get_option('permalink_structure')) {
+if (!is_multisite() && get_option('permalink_structure')) {
   if (current_theme_supports('rewrites')) {
     add_action('generate_rewrite_rules', 'roots_add_rewrites');
+    add_filter('mod_rewrite_rules', 'roots_add_conditions');
   }
 
   if (!is_admin() && current_theme_supports('rewrites')) {
