0e31e57cd39c247365e2007461aa5d782845062c
Joel Kuzmarski <leoj3n@gmail.com>
2013-07-22 12:38:46 -0500

    config.php defines add

--- lib/config.php
+++ lib/config.php
@@ -83,8 +83,11 @@ if (!isset($content_width)) { $content_width = 940; }
  * Define helper constants
  */
 $get_theme_name = explode('/themes/', get_template_directory());
+$get_child_name = explode('/themes/', get_stylesheet_directory());
 
 define('RELATIVE_PLUGIN_PATH',  str_replace(home_url() . '/', '', plugins_url()));
 define('RELATIVE_CONTENT_PATH', str_replace(home_url() . '/', '', content_url()));
 define('THEME_NAME',            next($get_theme_name));
 define('THEME_PATH',            RELATIVE_CONTENT_PATH . '/themes/' . THEME_NAME);
+define('CHILD_NAME',            next($get_child_name));
+define('CHILD_PATH',            RELATIVE_CONTENT_PATH . '/themes/' . CHILD_NAME);
